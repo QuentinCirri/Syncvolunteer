7137785a840ee6c7ee65b6638c155ecc63eb6543
Quatrecentvingt <149431210+YasmineSan@users.noreply.github.com>
2024-02-01 13:27:43 +0100

    java script
    page concernée connexion et inscription

--- JS/inscription.js
+++ JS/inscription.js
@@ -1,4 +1,13 @@
 function afficherFormulaire() {
     var formulaire = document.getElementById("formulaireInscription");
-    formulaire.style.display = "block";
+    formulaire.style.display = "flex";
 }
+document.addEventListener('DOMContentLoaded', function() {
+  
+
+    // Gestionnaire d'événement pour le bouton Connexion
+    document.getElementById('connexionButton').addEventListener('click', function() {
+        // Rediriger vers la page de connexion
+        window.location.href = 'connexion.html';
+    });
+});
