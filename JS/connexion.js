4aabb28d6e03fe71d96135a80783cc5c28df563b
Quatrecentvingt <149431210+YasmineSan@users.noreply.github.com>
2024-02-01 15:55:23 +0100

    connexion.html
    quand on se connecte on arrive sur calendrier

--- JS/connexion.js
+++ JS/connexion.js
@@ -2,3 +2,16 @@ document.getElementById('inscriptionButton').addEventListener('click', function(
     // Rediriger vers la page d'inscription
     window.location.href = '../page/inscription.html';
 });
+        document.getElementById("connexionForm").addEventListener("submit", function(event) {
+            event.preventDefault(); // Empêche l'envoi du formulaire
+
+            var email = document.getElementById("email").value;
+            var password = document.getElementById("password").value;
+
+            // regarder si ça fonctionne
+            console.log("Email: " + email);
+            console.log("Mot de passe: " + password);
+
+            // ajout d'une redirection de page (calendrier.html)
+            window.location.href = 'calendrier.html';
+        });
