df6ef56cf464eda22d6b47b8ce90bac714621261
Quatrecentvingt <149431210+YasmineSan@users.noreply.github.com>
2024-02-06 20:16:42 +0100

    modif javascript

--- JS/connexion.js
+++ JS/connexion.js
@@ -1,8 +1,5 @@
-document.getElementById('inscriptionButton').addEventListener('click', function() {
-    // Rediriger vers la page d'inscription
-    window.location.href = '../page/inscription.html';
-});
-        document.getElementById("connexionForm").addEventListener("submit", function(event) {
+
+         document.getElementById("connexionForm").addEventListener("submit", function(event) {
             event.preventDefault(); // Empêche l'envoi du formulaire
 
             var email = document.getElementById("email").value;
@@ -12,6 +9,5 @@ document.getElementById('inscriptionButton').addEventListener('click', function(
             console.log("Email: " + email);
             console.log("Mot de passe: " + password);
 
-            // ajout d'une redirection de page (calendrier.html)
             window.location.href = 'calendrier.html';
         });
